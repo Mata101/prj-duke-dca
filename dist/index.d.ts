f13b9414d013a56b8f00267d227e8690dbef50d3
Paul <pcmata@bai.ph>
2021-06-10 15:54:56 +0800

    initial project environment

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, PrjDukeDcaApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<PrjDukeDcaApplication>;
