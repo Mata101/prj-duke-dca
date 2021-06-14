088194b3eea0ace76e880d9cd0b3b42901138752
Paul <pcmata@bai.ph>
2021-06-14 11:25:25 +0800

    installed global node packages on local based on Parris' modern development javascript pre-requests and added also new node-modules on this initial project.

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, PrjDukeDcaApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<PrjDukeDcaApplication>;
